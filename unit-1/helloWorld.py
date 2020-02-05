22a25cbc9d8975997a140d090176e0c3c04c0025
Agung Tri A <agung.t.atmojo@gmail.com>
2020-02-05 15:03:20 +0700

    edit helloWorld.py

--- unit-1/helloWorld.py
+++ unit-1/helloWorld.py
@@ -1,4 +1,6 @@
 # My first Hello world
 
-print ("Hello World")
-print ("Saya sedang belajar bahasa Python")
+print("Hello World")
+print("Saya sedang belajar bahasa Python")
+print("belajar bahasa Python sungguh menyenangkan")
+print("Ayo kita belajar Python bersama-sama !")
