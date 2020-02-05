1782a45bdc7b4aa7c81c47a0287fa33a40db0b22
Agung Tri A <agung.t.atmojo@gmail.com>
2020-02-05 17:07:16 +0700

    add new file

---
+++ unit-1/dataType.py
@@ -0,0 +1,10 @@
+# Data type & variable in Python
+
+nama = "Agung"
+umur = 19
+nilai = 9.50
+
+print("Nama saya adalah : " + nama + ", tipe datanya adalah", type(nama))
+print("Umur saya sekarang : ", umur, " Tahun, tipe datanya adalah", type(umur))
+print("Nilai semester saya adalah : ", nilai, ", tipe datanya adalah", type(nilai))
+print("Umur saya di tahun depan adalah : ", umur+1, " Tahun")
