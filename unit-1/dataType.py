bbee3d9b10c4e22da08f7ffdabe8644cec62baf0
Agung Tri A <agung.t.atmojo@gmail.com>
2020-02-05 17:33:05 +0700

    add new examples

--- unit-1/dataType.py
+++ unit-1/dataType.py
@@ -3,8 +3,12 @@
 nama = "Agung"
 umur = 19
 nilai = 9.50
+keren = True
+tipe_List = ["str", "int", "float", "list", "tuple", "range", "dict", "set"]
 
 print("Nama saya adalah : " + nama + ", tipe datanya adalah", type(nama))
 print("Umur saya sekarang : ", umur, " Tahun, tipe datanya adalah", type(umur))
 print("Nilai semester saya adalah : ", nilai, ", tipe datanya adalah", type(nilai))
+print("Saya adalah orang yang keren ? ", keren, " , tipe datanya adalah", type(keren))
 print("Umur saya di tahun depan adalah : ", umur+1, " Tahun")
+print(tipe_List, "<- merupakan contoh dari tipe data list")
